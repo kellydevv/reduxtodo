6c0be314cb3c05d6f749f891ae2d568c0ca3b652
정현진 <jhj9328@hanmail.net>
2022-11-23 17:34:11 +0900

    dispatch action creator

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { minusOne, plusOne } from "./redux/modules/counter";
 
 const App = () => {
   const dispatch = useDispatch();
-  const number = useSelector ((state) => state.counter.number);
+  const number = useSelector((state) => state.counter.number);
   console.log(number);
   return (
     <div>
       {number}
-      <button onClick={() => {
-        	// 마우스를 클릭했을 때 dispatch가 실행되고, ()안에 있는 액션객체가 리듀서로 전달된다.
+      <button
+        onClick={() => {
+          // 마우스를 클릭했을 때 dispatch가 실행되고, ()안에 있는 액션객체가 리듀서로 전달된다.
           //{type:""}은 dispatch의 key(리듀서가 보는게 이거임)
-        dispatch({type:"PLUS_ONE"})
-      }}> +1 </button>
-      <button onClick={() => {dispatch({type:"MINUS_ONE"})}}>-1</button>
+          // export한 action  creator로 변경한다
+          dispatch(plusOne());
+        }}
+      >
+        {" "}
+        +1{" "}
+      </button>
+      <button
+        onClick={() => {
+          //dispatch({type:""})이지만 action creator를 만들었으므로 
+          //{type:"PLUS_ONE"}=== plusOne()
+          dispatch(minusOne());
+        }}
+      >
+        -1
+      </button>
     </div>
   );
 };
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -1,5 +1,21 @@
-//초기 상태값
-//=== useState(0);
+//액션value를 상수로 만들어준다.
+const PLUS_ONE = "PLUS_ONE";
+const MINUS_ONE = "MINUS_ONE";
+
+//action creator만들기
+export const plusOne = () =>{
+      return {
+            type:PLUS_ONE
+      };
+};
+
+export const minusOne = () => {
+      return {
+            type:MINUS_ONE
+      };
+};
+
+//초기 상태값 === useState(0);
 //초기값은 {}객체 안에 number이라는 변수에 초기값으로 0을 할당해준것.
 const initialState = {
   number: 0,
@@ -13,13 +29,13 @@ const counter = (state = initialState, action) => {
     // 여기서 말하는 case란, action.type을 의미한다.
     // dispatch로부터 전달받은 action의 type이 "PLUS_ONE" 일 때
     // 아래 return 절이 실행된다.
-    case "PLUS_ONE":
+    case PLUS_ONE: //위에 action creator를 만들었기때문에 문자열이 아닌 선언한 상수를 넣어줌
       return {
         // 기존 state에 있던 number에 +1을 더한다.
         number: state.number + 1,
       };
 
-      case "MINUS_ONE":
+      case MINUS_ONE:
             return {
                   number:state.number -1,
             };
