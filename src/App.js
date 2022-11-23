39c26bcab6708773d1ef6665494f88f19dc38c2b
정현진 <jhj9328@hanmail.net>
2022-11-23 17:03:46 +0900

    basic counter using redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
-  //useSelector 화살표함수에서 꺼낸 state라는 인자는 현재프로젝트에서 존재하는 모든 리덕스 모듈의 state이다.
-  const counterStore = useSelector((state) => state.counter.number)
-  console.log(counterStore);
-  return(
-<div>hihi</div>
-  )
+  const dispatch = useDispatch();
+  const number = useSelector ((state) => state.counter.number);
+  console.log(number);
+  return (
+    <div>
+      {number}
+      <button onClick={() => {
+        	// 마우스를 클릭했을 때 dispatch가 실행되고, ()안에 있는 액션객체가 리듀서로 전달된다.
+          //{type:""}은 dispatch의 key(리듀서가 보는게 이거임)
+        dispatch({type:"PLUS_ONE"})
+      }}> +1 </button>
+      <button onClick={() => {dispatch({type:"MINUS_ONE"})}}>-1</button>
+    </div>
+  );
 };
 
 export default App;
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -2,14 +2,30 @@
 //=== useState(0);
 //초기값은 {}객체 안에 number이라는 변수에 초기값으로 0을 할당해준것.
 const initialState = {
-      number:0,
+  number: 0,
 };
 
 //리듀서===변화를 일으키는 함수
 const counter = (state = initialState, action) => {
-      switch( action.type) {
-            default:return state;
-      }
+  console.log(action);
+  switch (action.type) {
+    // PLUS_ONE이라는 case를 추가한다.
+    // 여기서 말하는 case란, action.type을 의미한다.
+    // dispatch로부터 전달받은 action의 type이 "PLUS_ONE" 일 때
+    // 아래 return 절이 실행된다.
+    case "PLUS_ONE":
+      return {
+        // 기존 state에 있던 number에 +1을 더한다.
+        number: state.number + 1,
+      };
+
+      case "MINUS_ONE":
+            return {
+                  number:state.number -1,
+            };
+    default:
+      return state;
+  }
 };
 
 //모듈파일에서는 리듀서를 export default 한다.
